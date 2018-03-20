5a081c13f92c0918698d9b25bf70b096f5d20e23
huangwenhai <wenhai.hwh@alibaba-inc.com>
2018-03-20 19:08:08 +0800

    fix conflict

--- src/main/java/com/deep/api/authorization/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/deep/api/authorization/interceptor/AuthorizationInterceptor.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import redis.clients.jedis.Jedis;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
