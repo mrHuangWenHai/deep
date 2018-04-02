75875e74952dd8c6d708b72e56b7f3fc74d70532
doubibobo <15071267961@163.com>
2018-04-02 16:52:52 +0800

    2018-4-2-3

--- src/main/java/com/deep/api/resource/LoginResource.java
+++ src/main/java/com/deep/api/resource/LoginResource.java
@@ -203,8 +203,6 @@ public class LoginResource {
         if (userModel.getAnswer_1().equals(user.getAnswer_1()) &&
                 userModel.getAnswer_2().equals(user.getAnswer_2()) &&
                     userModel.getAnswer_3().equals(user.getAnswer_3())) {
-//            JedisUtil.setValue("Identify" + username,"Success");
-//            JedisUtil.doExpire("Identify" + username);
             user.setUserPwd(userModel.getUserPwd());
             Long updateID = userService.updateUser(user);
             if (updateID <= 0) {
@@ -215,29 +213,6 @@ public class LoginResource {
         }
         return Responses.errorResponse("问题答案错误!");
     }
-
-    // 只需要密碼
-    @PostMapping(value = "/repassword")
-    public Response RePassword(@RequestBody UserModel userModel){
-        if (!"Success".equals(JedisUtil.getValue("Identify" + userModel.getPkUserid()))){
-            return Responses.errorResponse("No Authtication");
-        }else {
-            UserModel user = userService.getUserByPkuserID(userModel.getPkUserid());
-            if (user == null) {
-                return Responses.errorResponse("error");
-            } else {
-                user.setUserPwd(userModel.getUserPwd());
-                Long updateID = userService.updateUser(user);
-                if (updateID <= 0) {
-                    return Responses.errorResponse("error!");
-                } else {
-                    return Responses.successResponse();
-                }
-            }
-        }
-    }
-
-
 
     /**
      * user logout for himself
