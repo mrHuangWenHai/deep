244210a5130164c24a29199ed89e2c98e5b3ad72
huangwenhai <wenhai.hwh@alibaba-inc.com>
2018-04-02 17:33:19 +0800

    fix

--- src/main/java/com/deep/api/request/LiveStatRequest.java
+++ src/main/java/com/deep/api/request/LiveStatRequest.java
@@ -1,8 +1,53 @@
 package com.deep.api.request;
 
-/**
- * Created by huangwenhai on 2018/4/2.
- */
-public class LiveStatRequest {
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
+
+/**
+ * Created by huangwenhai on 2018/3/14.
+ */
+
+public class LiveStatRequest {
+  @Min(value = 1)
+  int pageNo;
+
+  @Min(value = 1)
+  @Max(value = 300)
+  int pageSize;
+
+  String userId;
+  String pulldomain;
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public int getPageNo() {
+    return pageNo;
+  }
+
+  public void setPageNo(int pageNo) {
+    this.pageNo = pageNo;
+  }
+
+  public int getPageSize() {
+    return pageSize;
+  }
+
+  public void setPageSize(int pageSize) {
+    this.pageSize = pageSize;
+  }
+
+  public String getPulldomain() {
+    return pulldomain;
+  }
+
+  public void setPulldomain(String pulldomain) {
+    this.pulldomain = pulldomain;
+  }
 }
