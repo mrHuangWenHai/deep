b13692361f43ccd02dd465711408515c9280636e
huangwenhai <wenhai.hwh@alibaba-inc.com>
2018-04-12 23:29:31 +0800

    add cros :

--- src/main/java/com/deep/api/authorization/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/deep/api/authorization/interceptor/AuthorizationInterceptor.java
@@ -32,13 +32,14 @@ public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        System.out.println("dddddddddddddddddddddddddddddd");
         logger.info("invoke preHandle of AuthorizationInterceptor", request, response, handler);
         // 加相关的回应头
        // response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
-        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//        response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
+//        response.setHeader("Access-Control-Expose-Headers", "Authorization");
 
 
 //        if (request.getMethod().equals("OPTIONS")) {
