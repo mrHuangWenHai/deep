850bf838268b02dec2973a9dd1cc14a3f1db7f94
huangwenhai <wenhai.hwh@alibaba-inc.com>
2018-05-22 20:38:38 +0800

    fix redis bug

--- src/main/java/com/deep/api/Utils/JedisUtil.java
+++ src/main/java/com/deep/api/Utils/JedisUtil.java
@@ -1,14 +1,18 @@
 package com.deep.api.Utils;
 
 import com.deep.domain.service.ServiceConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
 public class JedisUtil {
     public static int seconds = 3600;
+    public static Logger logger = LoggerFactory.getLogger(JedisUtil.class);
 
     public static String getValue(String key) {
         Jedis jedis = RedisPool.getJedis();
         if (jedis == null) {
+            logger.info("jedis == null");
             return null;
         }
         try {
@@ -18,7 +22,15 @@ public class JedisUtil {
             return jedis.get(key);
           }
         } catch (Exception e ) {
+            logger.info("获取异常 {}",e.getMessage());
        //   jedis.close();
+        } finally {
+            try {
+                jedis.close();
+            } catch (Exception e) {
+                logger.info("关闭异常 {}",e.getMessage());
+            }
+
         }
         return null;
     }
@@ -29,7 +41,14 @@ public class JedisUtil {
             try {
                 jedis.setex(key, seconds, value);
             } catch (Exception e) {
-                e.printStackTrace();
+                logger.info("设置异常 {}",e.getMessage());
+            } finally {
+                try {
+                    jedis.close();
+                } catch (Exception e) {
+                    logger.info("关闭异常 {}",e.getMessage());
+                }
+
             }
         }
     }
@@ -41,7 +60,13 @@ public class JedisUtil {
             try {
                 jedis.expire(key,seconds);
             } catch (Exception e) {
-                e.printStackTrace();
+                logger.info("时间设置异常 {}",e.getMessage());
+            } finally {
+                try {
+                    jedis.close();
+                } catch (Exception e) {
+                    logger.info("关闭异常 {}",e.getMessage());
+                }
             }
         }
     }
@@ -51,10 +76,17 @@ public class JedisUtil {
         if (jedis != null) {
             try {
                 jedis.del(key);
-                System.out.println("Yes, it is");
+                logger.info("删除成功");
+
             } catch (Exception e) {
-                System.out.println("Something must be wrong!");
+                logger.info("删除异常 {}",e.getMessage());
                 return false;
+            } finally {
+                try {
+                    jedis.close();
+                } catch (Exception e) {
+                    logger.info("关闭异常 {}",e.getMessage());
+                }
             }
         }
         return true;
